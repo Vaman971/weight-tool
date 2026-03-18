fa2976c1c141c4fab7c48a819a6c5ccee604db61
vaman971 <vaman971@gmail.com>
2026-03-19 01:04:01 +0530

    fix: github link updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,7 +78,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "pydata_sphinx_theme"
 
 html_theme_options = {
-    "github_url": "https://github.com/airbus-amber/weight-function-calculator",
+    "github_url": "https://github.com/Vaman971/weight-tool",
     "use_edit_page_button": True,
     "navbar_start": ["navbar-logo"],
     "navbar_center": ["navbar-nav"],
